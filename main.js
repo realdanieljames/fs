f8be93900a75314673b66dc1dce58498b9c955c9
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-10-05 16:13:08 -0400

    fs filesync exercise

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
 // Create directory called fs-try
 
 // - Using fs module:
+const fs = require('fs');
 // - create a directory called content using fs method and it should log 'content folder created'
-// - create a file using fs method called randomText.txt that lives outside the content directory
-// - randomText.txt should be created using fs method and given just a short string of any data you want to put in it, using fs method
-// - when you create the randomText.txt file you should also log 'randomtext.txt created' in the terminal.
-// - write the randomText.txt data to a new file called verbage.txt inside the content folder and log 'verbage.txt created'
-// - Now create a separate setTimeout function that after 7 seconds deletes the content directory
+fs.mkdir('content', (err) => {
+    if (err) return console.log(err);
+    console.log('Content folder created')
+    
+    // - create a file using fs method called randomText.txt that lives outside the content directory
+    // - randomText.txt should be created using fs method and given just a short string of any data you want to put in it, using fs method
+    fs.writeFile('randomText.txt', 'well here is introducing another file created with the FS method writeFleSync', (err)=>{
+        if(err) return console.log(err)
+
+        // - when you create the randomText.txt file you should also log 'randomtext.txt created' in the terminal.
+        console.log('randomText.txt created')
+
+        // variable to hold file text to then past into different file.
+        const randomTextFile = fs.readFileSync('randomText.txt', 'utf-8')
+    
+        
+        // - write the randomText.txt data to a new file called verbage.txt
+        // inside the content folder and log 'verbage.txt created'
+        fs.writeFileSync('verbage.txt',randomTextFile )
+        
+        
+        // - Now create a separate setTimeout function that after 7 seconds
+        // deletes the content directory
+            setTimeout(()=>fs.rmdir('content',(err)=>{
+                if(err) throw err;
+                console.log('Content Directory removed')
+            }), 7000)
+        });
+    })
+
+
